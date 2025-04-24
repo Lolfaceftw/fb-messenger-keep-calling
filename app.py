38f0d07ac34f425f31ce8e6dbbce95be6a5c7381
unknown <ckcr20@gmail.com>
2025-04-24 23:31:30 +0800

    formatted code and removed TIMEOUT (unused)

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import pyautogui
 import time
 
-TIMEOUT = 5
 CONFIDENCE_LEVEL = 0.95
+
+
 def find_and_click(image_path: str) -> bool:
     while True:
         try:
             print(f"Finding {image_path}...")
-            location = pyautogui.locateCenterOnScreen(image_path, confidence=CONFIDENCE_LEVEL)
+            location = pyautogui.locateCenterOnScreen(
+                image_path, confidence=CONFIDENCE_LEVEL
+            )
             if location:
                 print(f"Found {image_path} @ {location}.")
                 print("Clicking...")
@@ -20,11 +23,12 @@ def find_and_click(image_path: str) -> bool:
         except Exception as e:
             print(f"Error! {e}")
 
+
 def main(*args, **kwargs) -> None:
     if find_and_click("video_cam.png") == True:
         while True:
             find_and_click("redial.png")
 
+
 if __name__ == "__main__":
     main()
-    
